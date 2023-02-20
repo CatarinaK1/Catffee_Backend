b793bd16f92075dd3c12b1ed276705e6dfb1f0aa
CatarinaK1 <catarinakaucher@gmail.com>
2023-02-20 14:36:55 +0200

    add

--- database/data.sql
+++ database/data.sql
@@ -90,6 +90,9 @@ ALTER TABLE `Newsletter`
   ADD PRIMARY KEY (`customerEmail`);
 COMMIT;
 
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 -----------------------*******************************************************************************************
 -------------****************************************************************************************************
 -------************************************************************************************************
