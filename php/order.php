8833ebcf821a7d574924ce8f6f830168954b6aab
Anil Khanal <66034315+makzibs@users.noreply.github.com>
2023-02-21 16:17:01 +0200

    adding

---
+++ php/order.php
@@ -0,0 +1,13 @@
+<?php $style = "order_style"; $title = "Catffee: order"; include "../php/header.php";
+?>
+<style>
+<?PHP include('../css/order_style.css'); ?> 
+</style>
+
+
+<h1> What are you craving for? </h1>
+
+
+
+   
+<?php include "../php/footer.php";?>
